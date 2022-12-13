2e37854fff7340e44e965b27b38426e5a96351ba
eduardonk9999 <thiago.novak7@gmail.com>
2022-12-13 08:54:40 -0300

    logica jump

--- main.js
+++ main.js
@@ -2,7 +2,24 @@ import Scenery from './src/components/Scenery';
 import './style.css'
 
 const $root = document.querySelector('#root');
+
+   
+
 const $htmlScenery = Scenery();
 
-$root.insertAdjacentHTML("beforeend", $htmlScenery)
+function jump() {
+    const luigi = document.querySelector('.luigi');
 
+    luigi.classList.add('jump')
+
+    setTimeout(() => {
+        luigi.classList.remove('jump')
+    }, 500)
+    console.log("te")
+}
+
+document.addEventListener('keydown', jump) 
+
+
+
+$root.insertAdjacentHTML("beforeend", $htmlScenery  )
--- src/components/Scenery/index.js
+++ src/components/Scenery/index.js
@@ -1,13 +1,19 @@
+
 import "./style.css";
 
+
+
 function Scenery() {
+ 
   return`
     <article class="game-board">
         <img class="luigi" src="./assets/luigirun.gif" alt="imagem animada do Luigi correndo" />
         <img class="pipe" src="./assets/mario-pipe.gif" alt="imagem do cano do jogo do Mario" />
     </article>
-  `
-}
+    
+    `
+  }
+ 
 
 
 export default Scenery;
