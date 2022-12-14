c3a5b2bb2b1a4cb1cb1d9102eea5d6f082aaabf5
eduardonk9999 <thiago.novak7@gmail.com>
2022-12-14 15:58:58 -0300

    feature: logica jump, e quando bate no obstaculo

--- main.js
+++ main.js
@@ -5,21 +5,40 @@ const $root = document.querySelector('#root');
 
    
 
+
 const $htmlScenery = Scenery();
+$root.insertAdjacentHTML("beforeend", $htmlScenery)
+
+const luigi = document.querySelector('.luigi');
+const pipe = document.querySelector('.pipe');
 
 function jump() {
-    const luigi = document.querySelector('.luigi');
 
     luigi.classList.add('jump')
 
     setTimeout(() => {
         luigi.classList.remove('jump')
     }, 500)
-    console.log("te")
 }
+
+ const loop = setInterval(() => {
+    const pipePosition = pipe.offsetLeft;
+    const luigiPosition = +window.getComputedStyle(luigi).bottom.replace('px', '')
+
+
+    console.log(pipePosition)
+
+    if(pipePosition <= 150  && pipePosition > 0 && luigiPosition < 50 ) {
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+    }
+
+}, 10);
+
+
+
+
+
 
 document.addEventListener('keydown', jump) 
 
-
-
-$root.insertAdjacentHTML("beforeend", $htmlScenery  )
--- src/components/Scenery/index.js
+++ src/components/Scenery/index.js
@@ -8,7 +8,7 @@ function Scenery() {
   return`
     <article class="game-board">
         <img class="luigi" src="./assets/luigirun.gif" alt="imagem animada do Luigi correndo" />
-        <img class="pipe" src="./assets/mario-pipe.gif" alt="imagem do cano do jogo do Mario" />
+        <img class="pipe" src="./assets/mario-pipe.gif " alt="imagem do cano do jogo do Mario" />
     </article>
     
     `
