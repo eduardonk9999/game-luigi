c93c85067976c2141c6b9bc9aaa307f4981006b4
eduardonk9999 <thiago.novak7@gmail.com>
2022-12-16 08:06:31 -0300

    logica game over

--- main.js
+++ main.js
@@ -26,11 +26,17 @@ function jump() {
     const luigiPosition = +window.getComputedStyle(luigi).bottom.replace('px', '')
 
 
-    console.log(pipePosition)
+    // console.log(pipePosition)
 
     if(pipePosition <= 150  && pipePosition > 0 && luigiPosition < 50 ) {
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;
+
+        luigi.src = './assets/over.gif';
+        luigi.style.animation = 'none';
+        luigi.style.bottom = `${pipePosition}px`;
+        luigi.classList.add('game-over')
+
     }
 
 }, 10);
